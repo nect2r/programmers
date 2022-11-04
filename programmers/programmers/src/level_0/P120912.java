cb27a3cdf174b87078bcca5d69690fd7ff9a8fbd
nect2r <nect2r@kakao.com>
2022-11-04 23:53:43 +0900

    집 커

---
+++ programmers/programmers/src/level_0/P120912.java
@@ -0,0 +1,20 @@
+package level_0;
+
+import java.util.*;
+
+public class P120912 {
+    class Solution {
+        public int solution(int[] array) {
+            int answer = 0;
+            String str = Arrays.toString(array);
+            
+            for(int i=0; i<str.length(); i++) {
+                if(str.charAt(i) == '7') {
+                    answer++;
+                }
+            }
+            
+            return answer;
+        }
+    }
+}
