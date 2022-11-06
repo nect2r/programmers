49604f5a18227c3e57f8998179b66da257a7f500
nect2r <nect2r@kakao.com>
2022-11-06 23:36:46 +0900

    집!

---
+++ programmers/programmers/src/level_0/P120835.java
@@ -0,0 +1,22 @@
+package level_0;
+
+public class P120834 {
+    class Solution {
+    public int[] solution(int[] emergency) {
+        int[] answer = new int[emergency.length];
+        
+        for(int i=0; i<emergency.length; i++) {
+            int rank = 1;
+            for(int j=0; j<emergency.length; j++) {
+                if(emergency[i] < emergency[j]) {
+                    rank++;
+                }
+            }
+            answer[i] = rank;
+        }
+        
+        
+        return answer;
+    }
+}
+}
