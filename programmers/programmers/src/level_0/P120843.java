b7695756e9f22e1a1b2336e15a25a01d5b908a0e
nect2r <nect2r@kakao.com>
2022-11-12 22:09:46 +0900

    집 커밋

---
+++ programmers/programmers/src/level_0/P120843.java
@@ -0,0 +1,10 @@
+package level_0;
+
+public class P120843 {
+    class Solution {
+        public int solution(int[] numbers, int k) {
+            int answer = 0;
+            return answer;
+        }
+    }
+}
